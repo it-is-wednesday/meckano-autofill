35f70ddcd09bc24dd3d5f4e9a03a616491b90c22
Maor Kadosh <git@avocadosh.xyz>
2023-07-08 09:05:11 +0300

    tread only selected days

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 (() => {
+    /// Only these days will be treated.
+    /// 0 = Sunday, 1 = Monday, ..., Friday = 5, Saturday = 6
+    const DAYS_TO_FILL = [1, 3];
+    
     const rows =  Array.from(document.querySelectorAll('.hours-report tr'));
+    
     // first row is the table header
     for(let i = 1; i< rows.length; i++) {
-
-        // this is a weekend!
-        if (rows[i].querySelector('td:first-child').innerText.match(/[וש]/)) { 
-        continue;
-        }
-
-        // skip FRI / SAT
+        // Find the day this row represents
         const dateStr = rows[i].querySelector('.dateText').innerText
         const dateArr = dateStr.split(' ')[0].split('/')
-        const date = new Date(dateArr[2], parseInt(dateArr[1]) - 1, dateArr[0])
-        if (date.getDay() >= 5) {
-            continue;
+        const day = new Date(dateArr[2], parseInt(dateArr[1]) - 1, dateArr[0]).getDay()
+        
+        // Fill if we're in a day to fill
+        if (DAYS_TO_FILL.includes(day)) {
+            rows[i].querySelector('.checkIn input').value = '9:30'
+            rows[i].querySelector('.checkOut input').value = '18:00';
         }
-        rows[i].querySelector('.checkIn input').value = '9:00'
-        rows[i].querySelector('.checkOut input').value = '18:00'
-        } 
-
+    } 
 })();
